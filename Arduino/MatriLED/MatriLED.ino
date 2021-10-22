8eca5c6fd8f541047cde3365d88856ad1123b4f3
Corentin Hervaud <corentin.hervaud@epitech.eu>
2021-10-22 23:05:25 +0200

    Change displayed text

--- Arduino/MatriLED/MatriLED.ino
+++ Arduino/MatriLED/MatriLED.ino
@@ -7,7 +7,7 @@ void setup(void)
 {
     Serial.begin(9600);
     Matrice.Text.Align = Center;
-    Matrice.Text.Text = "Hello";
+    Matrice.Text.Text = "Hello World !";
     Matrice.Text.Scroll_Speed = SCROLL_MEDIUM;
     Init_Matrices(1);
     (Matrice.ContentInfo[0]).ContentType = Text;
