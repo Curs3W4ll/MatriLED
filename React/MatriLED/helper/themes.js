a3ac7d09649b555a3aab7634d9854d39f07f4519
Corentin Hervaud <corentin.hervaud@epitech.eu>
2021-11-20 15:57:41 +0100

    feat(color theme): Add new color themes #36

--- React/MatriLED/helper/themes.js
+++ React/MatriLED/helper/themes.js
@@ -1,27 +1,43 @@
-export const darkTheme = {
+export var darkTheme = {
   id: 'dark',
-  background: 'black',
-  text: 'white',
-  main: '#c930e5',
-  sub: '#dbbbe1',
-  annex: '#797272',
-  dangerMain: '#ff0000',
-  dangerSub: '#ea9494',
   statusBar: 'light',
   tabBar: 'dark',
   themeIcon: 'moon',
-};
-
-export const lightTheme = {
-  id: 'light',
-  background: 'white',
-  text: 'black',
-  main: '#c930e5',
-  sub: '#dbbbe1',
-  annex: '#cccccc',
+  background: 'black',
+  text: 'white',
+  annex: '#797272',
   dangerMain: '#ff0000',
   dangerSub: '#ea9494',
+};
+
+export var lightTheme = {
+  id: 'light',
   statusBar: 'dark',
   tabBar: 'light',
   themeIcon: 'moon-outline',
+  background: 'white',
+  text: 'black',
+  annex: '#cccccc',
+  dangerMain: '#ff0000',
+  dangerSub: '#ea9494',
+};
+
+export const purpleTheme = {
+  main: '#c930e5',
+  sub: '#99839e',
+};
+
+export const cyanTheme = {
+  main: '#08e8be',
+  sub: '#07735f',
+};
+
+export const redTheme = {
+  main: '#da101a',
+  sub: '#741b1f',
+};
+
+export const blueTheme = {
+  main: '#2f0dea',
+  sub: '#241288',
 };
